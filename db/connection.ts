c77d8f681e6c8e0873e8c286da42c4cdb74bb8f5
Anthony Cochea <thonydevelopersoftware@gmail.com>
2023-07-08 15:01:20 -0500

    Seccion de auditSafe Consulting

--- db/connection.ts
+++ db/connection.ts
@@ -1,7 +1,10 @@
+import dotenv from "dotenv";
 import { Sequelize } from "sequelize";
 
+//Configuración de las variables de entorno
+dotenv.config();
 
-const db = new Sequelize('Scrapper', 'DatabaseAdmin', 'admin123456',{
+const db = new Sequelize('Scrapper', process.env.USER_DATABASE ||'', process.env.PASSWORD_DATABASE || '',{
     host: 'localhost',
     dialect: 'mssql',
     
--- dist/db/connection.js
+++ dist/db/connection.js
@@ -1,7 +1,13 @@
 "use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
 Object.defineProperty(exports, "__esModule", { value: true });
+const dotenv_1 = __importDefault(require("dotenv"));
 const sequelize_1 = require("sequelize");
-const db = new sequelize_1.Sequelize('Scrapper', 'DatabaseAdmin', 'admin123456', {
+//Configuración de las variables de entorno
+dotenv_1.default.config();
+const db = new sequelize_1.Sequelize('Scrapper', process.env.USER_DATABASE || '', process.env.PASSWORD_DATABASE || '', {
     host: 'localhost',
     dialect: 'mssql',
 });
